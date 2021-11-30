19f2c3e3ee9e57680f16f943fb6fad85d86e33db
unknown <slava-suhanov@yandex.ru>
2021-11-30 08:17:26 +0300

    Fixed 3/8

--- Lab3/8.sql
+++ Lab3/8.sql
@@ -1,5 +1,6 @@
 ﻿/*8. Показать список BusinessEntityID, которые содержатся и в таблице Person.Person, и в таблице HumanResources.EmployeePayHistory*/
-SELECT Distinct Person.Person.BusinessEntityID
+SELECT Person.Person.BusinessEntityID
 FROM Person.Person
-LEFT OUTER JOIN HumanResources.EmployeePayHistory
-On Person.Person.BusinessEntityID=HumanResources.EmployeePayHistory.BusinessEntityID
+INTERSECT
+SELECT BusinessEntityID
+FROM HumanResources.EmployeePayHistory
--- Lab4/5.sql
+++ Lab4/5.sql
@@ -1,16 +1,5 @@
 ﻿/*5. Показать товар с максимальной разницей Стоимости последнего поступления и Стандартной стоимости (Таблица Production.Product)
 (ПРИМЕЧАНИЕ! В таблице Production.Product нет данных о стоимости последнего поступления. Возможно имелась в виду таблица Purchasing.ProductVendor?)*/
-/*SELECT ProductID AS Diff
-FROM Production.Product
-WHERE (ProductID) = (SELECT MAX(ProductID) FROM Purchasing.ProductVendor WHERE (ABS(LastReceiptCost-Production.Product.StandardCost))=MAX(ABS(LastReceiptCost-Production.Product.StandardCost)))*/
-
-SELECT Production.Product.ProductID, MAX(ABS(Vendor.LastReceiptCost-StandardCost)) AS Difference FROM Production.Product
-Right outer join Purchasing.ProductVendor as Vendor
-On ABS(LastReceiptCost-Production.Product.StandardCost)=(select MAX(ABS(LastReceiptCost-Production.Product.StandardCost)) from Purchasing.ProductVendor)
-WHERE StandardCost > 0
-
-/*Select LastReceiptCost, StandardPrice from Purchasing.ProductVendor*/
-
-Select * from Production.Product
-
-Select * from Purchasing.ProductVendor
+SELECT ProductID, (ABS(LastReceiptCost-StandardPrice)) AS Diff
+FROM Purchasing.ProductVendor
+WHERE (ABS(LastReceiptCost-StandardPrice) = (SELECT MAX(ABS(LastReceiptCost-StandardPrice)) FROM Purchasing.ProductVendor))
